de19da3c157e74b160c7cc32ea105b326f4f693e
alissonmoreira7 <alonmoreira.dev@gmail.com>
2026-02-15 21:34:10 -0300

    Finalização da função que adiciona a coluna de verificação de se o registro é no periodo de carnaval ou não

--- src/etl/tratador.py
+++ src/etl/tratador.py
@@ -10,3 +10,21 @@ def transformar_dados(df):
     df['id'] = df['id'].astype(int)
 
     return df
+
+# Adicionar colunas úteis como verificação para saber se é carnaval ou não
+def adicionar_colunas(df):
+    #Periodo de carnaval nos respectivos anos: 2023, 2024 e 2025
+    periodo_carnaval = [
+        ('2023-02-17', '2023-02-22'),
+        ('2024-02-09', '2024-02-14'),
+        ('2025-02-28', '2025-03-05')
+    ]
+
+    df['carnaval'] = 0
+
+    for inicio, fim in periodo_carnaval:
+        filtro = (df['data_inversa'] >= inicio) & (df['data_inversa'] <= fim)
+
+        df.loc[filtro, 'carnaval'] = 1
+
+    return df
